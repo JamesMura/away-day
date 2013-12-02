36903247f5728eb2e1b6f2082088eb6ac6cb05ba
Faris Mohammed <farismosman@gmail.com>
2013-12-02 16:16:44 +0300

    Faris | Created Rails template.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AwaydayApp::Application.config.secret_token = '589213c98faee7adb1fbca24403f0fe33589f7898330cce991231e47de196fadda6127cad4e732daf2302c6d2945bb28564f83c9294a4e7f24b8e57db13bb5d5'
